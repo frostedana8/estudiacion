59f2e4fe09d9ddbdb2f6d0bb5d543b61e441ea86
Nana <36996739+frostedana8@users.noreply.github.com>
2022-11-22 14:22:14 +0100

    Add files via upload

---
+++ accesibilidad/script.js
@@ -0,0 +1,21 @@
+function verde(){
+
+    document.querySelector("#fondo").style = "background-color: greenyellow;"
+
+}
+
+
+function azul(){
+
+    document.querySelector("#fondo").style = "background-color: lightblue;"
+
+
+}
+
+
+ function naranja(){
+
+    document.querySelector("#fondo").style = "background-color: orange;"
+
+
+}
